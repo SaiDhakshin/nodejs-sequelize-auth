9f7fd775db0b971beac4525689349af62e1fd2a2
Saidhakshin <Saidhakshin75@gmail.com>
2021-10-15 17:18:46 +0530

    SSL fix

--- util/sequelize.js
+++ util/sequelize.js
@@ -11,6 +11,7 @@ const Sequelize = require('sequelize');
 // })
 
 const sequelize = new Sequelize(process.env.DATABASE_URL,{
+  ssl : true,
   dialect: 'postgres',
   storage : './session.postgres',
   dialectOptions: {
