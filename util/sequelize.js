7acbf9d30a9e9eddae56810827beacbb4f032b96
Saidhakshin <Saidhakshin75@gmail.com>
2021-10-15 13:32:16 +0530

    Configured to my mac.

--- util/sequelize.js
+++ util/sequelize.js
@@ -1,13 +1,22 @@
 const Sequelize = require('sequelize');
 
 
-const sequelize = new Sequelize('authuser','postgres','qmpzfgh4563',{
+const sequelize = new Sequelize('authuser','saidhakshin','qmpzfgh4563',{
     dialect: 'postgres',
     host : 'localhost',
     storage : './session.postgres',
     dialectOptions: {
       supportBigNumbers: true
     }
+})
+
+const sequelize = new Sequelize(process.env.PG_DATABASE,process.env.PG_USER, process.env.PG_PASSWORD,{
+  dialect: 'postgres',
+  host : process.env.PG_HOST,
+  storage : './session.postgres',
+  dialectOptions: {
+    supportBigNumbers: true
+  }
 })
 
 try {
