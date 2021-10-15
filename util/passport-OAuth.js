d562d5cfac607e80b429d4c1dc18f7f53d4237b3
Saidhakshin <Saidhakshin75@gmail.com>
2021-10-15 17:44:26 +0530

    Fixing google callback2

--- util/passport-OAuth.js
+++ util/passport-OAuth.js
@@ -14,7 +14,7 @@ passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     // callbackURL: "https://localhost:3000/auth/google/callback",
-    callbackURL: "https://boiling-thicket-23526.herokuapp.com/auth/google/callback",
+    callbackURL: "/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
