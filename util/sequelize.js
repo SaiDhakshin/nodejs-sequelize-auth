d3b683c93f76a421b3a03a6f45385c79b496afcd
Saidhakshin <Saidhakshin75@gmail.com>
2021-10-15 17:23:59 +0530

    SSL fix3

--- util/sequelize.js
+++ util/sequelize.js
@@ -15,6 +15,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL,{
   storage : './session.postgres',
   dialectOptions: {
     ssl : true,
+    rejectUnauthorized: false,
     supportBigNumbers: true
   }
 })
