b07f303c65a926b32e5cfa23cf20f4ac87e3a490
Saidhakshin <Saidhakshin75@gmail.com>
2021-10-15 16:49:25 +0530

    GOauth fixing .

--- util/database.js
+++ util/database.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_DATABASE,
-    host : process.env.DB_HOST,
-    port : process.env.DB_PORT
+    user : process.env.PG_USER,
+    password : process.env.PG_PASSWORD,
+    database : process.env.PG_DATABASE,
+    host : process.env.PG_HOST,
+    port : process.env.PG_PORT
 })
 
 module.exports = pool;
